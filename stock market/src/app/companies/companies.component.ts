b94d23cece03bf4c8d108b06eeb8f1ba10ea0f00
aneena04 <59555003+aneena04@users.noreply.github.com>
2020-02-05 18:03:54 +0530

    project feb 5

---
+++ stock market/src/app/companies/companies.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from '../company.service';
+import { Router } from '@angular/router';
+import { Company } from '../model/company';
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.css']
+})
+export class CompaniesComponent implements OnInit {
+  companies: Company[];
+  constructor(private companyService: CompanyService, private router: Router) { }
+
+  ngOnInit() {
+
+    this.companyService.getAllCompany().subscribe(data => {
+      this.companies = data;
+    });
+
+
+  }
+  deleteCompany(company:Company){
+    this.companyService.deleteCompany(company.id).subscribe();
+    this.companies=this.companies.filter(c=> c !== company);
+    }
+}
---
+++ stock market/src/app/model/company.ts
@@ -0,0 +1,11 @@
+export class Company{
+  static id(id: any) {
+    throw new Error("Method not implemented.");
+  }
+    id:number;
+    company:string;
+    cffice:string;
+    turnover:number;
+    ceo:string;
+    
+}
